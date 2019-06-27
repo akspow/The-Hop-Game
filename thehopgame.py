4be9d8de39fd36a2d9e0cc71dd2072756a28aec6
Akshay Chandru <31569582+akspow@users.noreply.github.com>
2019-06-27 20:00:02 +0530

    Update thehopgame.py

--- thehopgame.py
+++ thehopgame.py
@@ -1,30 +1,36 @@
 n=int(input())
 p=list(map(int,input().split(',')))
-def res(arr,n):
-    max=0
-    pos=-1
+def res(p,n,pos):
+    array=[0,0,0]
     dc=1
-    a=p[pos+1]
-    if(pos>=n-1):
-        return max
-    if(pos+2>n-1):
-        max+=n
-        return max
-    if(pos+3>n-1):
-        max+=h
-    b=2*p[pos+2]
-    c=3*p[pos+3]
-    if(a>b and a>c):
-        pos+=1
-        max+=a
-        res(arr[pos:],n)
-    elif(b>c):
-        pos+=2
-        max+=b
-        res(arr[pos+2:],n)
-    else:
-        if dc>0:
-           max+=c
-           dc=0
-           res(arr[pos+3:],n)
-print(res(p,n))
+    mx=0
+    for i in range(n):
+        if(pos>=n-1):
+            return mx
+        if(pos+1>=n-1):
+            mx+=p[pos+1]
+            return mx
+        if(pos+2>=n-1):
+            mx+=max(p[pos+1]+p[pos+2],2*p[pos+2])
+            return mx
+        array[2]=p[pos+3]*3
+        array[1]=p[pos+2]*2+p[pos+3]
+        array[0]=max(p[pos+1]+p[pos+2]+p[pos+3],p[pos+1]+2*p[pos+3])
+        if(max(array)==array[2]):
+            if(dc>0):
+                    pos+=3
+                    dc=0
+                    mx+=array[2]
+                    res(p,n,pos)
+            else:
+                array[2]=0
+        if(max(array)==array[1]):
+            pos+=3
+            mx+=array[1]
+            res(p,n,pos)
+        if(max(array)==array[0]):
+            pos+=3
+            mx+=array[0]
+            res(p,n,pos)
+maximum=res(p,n,-1)
+print(maximum)
